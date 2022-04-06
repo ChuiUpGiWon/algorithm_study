06840eec17df004bfd9d0aba87009c800b26dcc8
Seung Won <50093199+s2eung1@users.noreply.github.com>
2022-04-06 17:43:28 +0900

    [BOJ] 2530번 인공지능 시계 / Bronze 4

--- BOJ SeungWon/2530.py
+++ BOJ SeungWon/2530.py
@@ -1,19 +1,19 @@
 h,m,s=map(int,input().split())
 t=int(input())
 
-s+=t%60
+s+=t%60 //초계산
 t=t//60
 if s>=60:
     m+=1
     s-=60
 
-m+=t%60
+m+=t%60 //분계산
 t=t//60
 if m>=60:
     h+=1
     m-=60
 
-h+=t%24
+h+=t%24 //시계
 if h>=24:
     h-=24
 
